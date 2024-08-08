06fc05e0e9aa9f0216ecdf4920e25779233912f6
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-08 16:25:26 -0400

    test hashset

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -103,4 +103,34 @@ public class TestStack {
         map.clear();
         Assert.assertEquals(0, map.size());
     }
+
+    // Test HashSet
+    @Test
+    public void testHashSetAdd(){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        Assert.assertTrue(set.contains(1));
+    }
+
+    @Test
+    public void testHashSetContains(){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        Assert.assertTrue(set.contains(1));
+    }
+
+    @Test
+    public void testHashSetSize(){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        Assert.assertEquals(1, set.size());
+    }
+
+    @Test
+    public void testHashSetClear(){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.clear();
+        Assert.assertEquals(0, set.size());
+    }
 }
