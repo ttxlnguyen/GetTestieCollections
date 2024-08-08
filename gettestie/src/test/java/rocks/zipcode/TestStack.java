a021f0838a9b8854c39bda969d43f51b47caf7bf
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-08 16:28:23 -0400

    test vector

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -260,4 +260,18 @@ public class TestStack {
         t.add("a");
         Assert.assertEquals("b", t.last());
     }
+
+    // Test Vector
+    @Test
+    public void testCap(){
+        Vector<String> v = new Vector<>();
+        Assert.assertEquals(10, v.capacity());
+    }
+
+    @Test
+    public void testEnsureCap(){
+        Vector<String> v = new Vector<>();
+        v.ensureCapacity(21);
+        Assert.assertEquals(21, v.capacity());
+    }
 }
