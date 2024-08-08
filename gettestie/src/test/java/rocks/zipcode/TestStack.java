71f787166405ebbfda29167d799326fdcc01c9ea
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-08 16:26:29 -0400

    test linkedlist

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -163,4 +163,22 @@ public class TestStack {
         }
         Assert.assertEquals(3,i);
     }
+
+    // Test LinkedList
+    @Test
+    public void testLinkedListAddFirst(){
+        LinkedList<String> list = new LinkedList<>();
+        list.add("a");
+        list.addLast("b");
+        Assert.assertEquals("a", list.getFirst());
+    }
+
+    @Test
+    public void testLinkedListRemoveFirst(){
+        LinkedList<String> list = new LinkedList<>();
+        list.add("a");
+        list.add("b");
+        list.remove("a");
+        Assert.assertEquals("b", list.getFirst());
+    }
 }
