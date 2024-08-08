b5389a955e397120998a59211f4f11e507fbc88b
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-08 16:26:07 -0400

    test iterator

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -133,4 +133,34 @@ public class TestStack {
         set.clear();
         Assert.assertEquals(0, set.size());
     }
+
+    // Test Iterator
+    @Test
+    public void iteratorNext(){
+        ArrayList<String> letters = new ArrayList<String>();
+        letters.add("a");
+        letters.add("b");
+
+        Iterator<String> it = letters.iterator();
+        String expected = "a";
+        String next = it.next();
+        Assert.assertEquals(expected, next);
+    }
+
+    @Test
+    public void iteratorHasNext(){
+        int i = 0;
+
+        ArrayList<String> letters = new ArrayList<String>();
+        letters.add("a");
+        letters.add("b");
+        letters.add("c");
+
+        Iterator<String> it = letters.iterator();
+        while (it.hasNext()) {
+            it.next();
+            i += 1;
+        }
+        Assert.assertEquals(3,i);
+    }
 }
